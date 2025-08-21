bb63ed81009e2cc5e1cc6a47af7636af658a0114
anushwetmalvi <anushwetmalvi@gmail.com>
2025-08-21 18:45:28 +0530

    inicial commit

---
+++ M8/Classwork/l1-A1.py
@@ -0,0 +1,16 @@
+#printing a greeting message
+print("Hello, world!")
+
+#printing my name
+print("My name is Anushwet Malvi.")
+
+#printing a fun fact about myself
+print("I love playing cricket.")
+
+#performing and printing a calculation
+print("The result of 5 + 3 is:", 5 + 3)
+
+'''
+this is
+ a multi-line comment
+'''
---
+++ M8/Classwork/l1-A4.py
@@ -0,0 +1,12 @@
+#print a sentence
+print("Welcome to the world of programming!")
+
+#print a number
+print(4)
+
+#next line after print
+print("hello python \n\n\nThis sentence is coming in the new line.")
+
+#print all the keywords present in Python print("Keywords in Python are..........")
+import keyword
+print(keyword.kwlist)
