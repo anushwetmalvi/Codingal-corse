49b2b244ddf37027f377f41d43ade9fe25ea9567
anushwetmalvi <anushwetmalvi@gmail.com>
2025-08-26 18:49:58 +0530

    inicial commit

---
+++ M8/Classwork/l2-A3.py
@@ -0,0 +1,21 @@
+first_name = "codingal"
+last_name = "Education"
+full_name = first_name + " " + last_name
+example = "Haa"*5
+
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Full Name:", full_name)
+print("String multiplied 5 times:", example)
+
+word = 'Coding'
+print("length of word:", len(word))
+print("first character:", word[0])
+print("last character:", word[5])
+print("slice from 1 to 4:", word[1:5])
+
+'''
+C O D I N G
+0 1 2 3 4 5
+-6 -5 -4 -3 -2 -1
+'''
