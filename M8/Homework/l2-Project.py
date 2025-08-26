d00db0c3251c897f5577d48bb3e550bdfe9dfc07
anushwetmalvi <anushwetmalvi@gmail.com>
2025-08-26 19:02:23 +0530

    inicial commit

---
+++ M8/Homework/l2-Project.py
@@ -0,0 +1,15 @@
+# take input values from user
+x = input("enter Value of x: ")
+y = input("enter Value of y: ")
+z = input("enter Value of z: ")
+
+# swapping
+temp = x
+x = y
+y = z
+z = temp
+
+# displaying results after swapping
+print("Value of x after swapping:", x)
+print("Value of y after swapping:", y)
+print("Value of z after swapping:", z)
