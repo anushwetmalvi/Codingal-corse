7e595d50c853a4249ce982fa0fe48e5cb06e1abb
anushwetmalvi <anushwetmalvi@gmail.com>
2025-08-26 18:58:07 +0530

    inicial commit

---
+++ M8/Classwork/l2-A4.py
@@ -0,0 +1,15 @@
+# take input values from user
+x = input("enter Value of x:")
+y = input("enter Value of y:")
+
+#swapping
+temp = x
+x = y
+y = temp
+
+#single line swapping
+#x, y = y, x
+
+#displaying results after swapping
+print("Value of x after swapping:", x)
+print("Value of y after swapping:", y)
