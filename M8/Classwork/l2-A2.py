fe829b27340d401a9601aef9cee220037f376648
anushwetmalvi <anushwetmalvi@gmail.com>
2025-08-26 18:32:34 +0530

    inicial commit

---
+++ M8/Classwork/l2-A2.py
@@ -0,0 +1,21 @@
+num1 = 45
+num2 = 3
+# 45 = 4*11+1 ; 0<=r<a
+print("number 1:", num1)
+print("number 2:", num2)
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Floor Division:", num1 // num2)
+print("Modulus:", num1 % num2)
+print("square:", num2 ** 2)
+print("square root:", num1 ** 0.5)
+
+print("equal ?", num1 == num2)
+print("not equal ?", num1 != num2)
+print("greater than ?", num1 > num2)
+print("less than ?", num1 < num2)
+
+result = num1/2+num2**2+10
+print("result is:", result)
