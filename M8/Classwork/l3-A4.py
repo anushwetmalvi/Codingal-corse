ec85a6a053a467fc73bb139877b3276a56f6d1fc
anushwetmalvi <anushwetmalvi@gmail.com>
2025-09-01 18:29:33 +0530

    inicial commit

---
+++ M8/Classwork/l3-A4.py
@@ -0,0 +1,10 @@
+import datetime, calendar
+
+# using now() to get current time
+current_time = datetime.datetime.today()
+
+# Printing value of now
+print("Time now at greenwich meridian is : ", current_time)
+
+# print calendar of year 2025
+print("\n", calendar.calendar(2025))
