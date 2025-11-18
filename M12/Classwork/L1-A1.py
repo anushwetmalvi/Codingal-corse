1adf81776e3e0123b9e46551f66124e9d27ef90f
anushwetmalvi <anushwetmalvi@gmail.com>
2025-11-18 18:32:07 +0530

    inicial commit

---
+++ M12/Classwork/L1-A1.py
@@ -0,0 +1,33 @@
+num1 = 10
+num2 = 5
+
+print("num1 & num2 is-", num1, num2)
+print("num1 & num2 are-", num1 & num2)  # AND operator
+print("num1 | num2 are-", num1 | num2)  # OR operator
+print("num1 ^ num2 are-", num1 ^ num2)  # XOR operator
+print("num1 << 2 is-", num1 << num2)    # left shift operator
+print("num2 >> 2 is-", num1 >> num2)    # right shift
+print("~num1 is-", ~num1)               # negation operator
+print("~num2 is-", ~num2)               # negation operator
+
+
+
+'''
+1 or 1 = 1
+1 or 0 = 1
+0 or 1 = 1
+0 or 0 = 0
+
+1 AND 1 = 1
+1 AND 0 = 0
+0 AND 1 = 0
+0 AND 0 = 0
+
+1 XOR 1 = 0
+0 XOR 0 = 0
+1 XOR 0 = 1
+0 XOR 1 = 1
+
+not 1 = 0
+not 0 = 1
+'''
